ee01b1b36eb18c93d06ac3a30f5838a3d889135b
frkri <57220519+frkri@users.noreply.github.com>
2023-11-07 04:56:46 +0100

    Add Protobuffers library & pre build event

--- CSharpServer/Client.cs
+++ CSharpServer/Client.cs
@@ -1,6 +1,12 @@
-﻿namespace CSharpServer;
+﻿using Croissantbit;
+
+namespace CSharpServer;
 
 public class Client
 {
-    
+    public Client()
+    {
+        var Ping = new Croissantbit.Ping();
+        Console.WriteLine(Ping);
+    }
 }
